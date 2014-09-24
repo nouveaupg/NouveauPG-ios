9b0dc74145fd790f2ff951bac2e35898c73adcfa
John Hill <jack@johns-mbp.home>
2014-09-24 19:35:02 -0400

    1.2 final UI

---
+++ NouveauPG/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  NouveauPG
+//
+//  Created by John Hill on 9/24/14.
+//  Copyright (c) 2014 John Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ NouveauPG/SettingsViewController.m
@@ -0,0 +1,64 @@
+//
+//  SettingsViewController.m
+//  NouveauPG
+//
+//  Created by John Hill on 9/24/14.
+//  Copyright (c) 2014 John Hill. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController () {
+    IBOutlet UISegmentedControl *m_hashAlgoControl;
+    IBOutlet UISegmentedControl *m_rsaKeySizeControl;
+    IBOutlet UISwitch *m_enableKeychainSwitch;
+    IBOutlet UISwitch *m_enableTouchIdSwitch;
+}
+
+- (IBAction)changeSetting:(id)sender;
+
+@end
+
+@implementation SettingsViewController
+
+- (IBAction)changeSetting:(id)sender {
+    if (sender == m_enableKeychainSwitch) {
+        [[NSUserDefaults standardUserDefaults] setBool:m_enableKeychainSwitch.on forKey:@"enableKeychain"];
+    }
+    else if( sender == m_enableTouchIdSwitch ) {
+        [[NSUserDefaults standardUserDefaults] setBool:m_enableTouchIdSwitch.on forKey:@"enableTouchId"];
+    }
+    else if( sender == m_hashAlgoControl ) {
+        [[NSUserDefaults standardUserDefaults] setInteger:m_hashAlgoControl.selectedSegmentIndex forKey:@"hashAlgo"];
+    }
+    else if( sender == m_rsaKeySizeControl ) {
+        [[NSUserDefaults standardUserDefaults] setInteger:m_rsaKeySizeControl.selectedSegmentIndex forKey:@"rsaKeySize"];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    m_enableKeychainSwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"enableKeychain"];
+    m_enableTouchIdSwitch.on = [[NSUserDefaults standardUserDefaults]boolForKey:@"enableTouchId"];
+    m_hashAlgoControl.selectedSegmentIndex = [[NSUserDefaults standardUserDefaults]integerForKey:@"hashAlgo"];
+    m_rsaKeySizeControl.selectedSegmentIndex = [[NSUserDefaults standardUserDefaults]integerForKey:@"rsaKeySize"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
